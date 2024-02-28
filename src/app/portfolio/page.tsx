a96c54a7cd6a80a2060744356628e43cd347d6b7
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-02-28 06:08:12 +0600

    user sign in

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -7,8 +7,10 @@ import Loader from "@/components/Loader";
 import { scrollToTop } from "../../../utils/shared";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useCurrentUser } from "../../components/hooks/useCurrentUser";
 
 const Page = () => {
+  const currentUser = useCurrentUser();
   const { state } = FetchData("/data.json");
   const { data } = state;
   const [isLoading, setIsLoading] = useState(true);
@@ -39,7 +41,90 @@ const Page = () => {
   const portfolio = data?.portfolioData;
 
   return (
-    <div className="h-full w-full relative">
+    <>
+    {currentUser ? (
+      <div className="h-full w-full relative">
+        <Loader isLoading={isLoading} />
+
+        {!isLoading && data && (
+          <section
+            className={` ${
+              !isLoading && data ? "display-block" : "display-none"
+            } font-Spartan px-0 ss:px-10 md:px-16 md:container w-full h-full text-dark relative`}
+          >
+            <Header />
+
+            <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 gap-7 px-[30px] ss:px-16 md:px-20 mb-[200px] relative">
+              {portfolio?.map((item: any, index: number) => {
+                return (
+                  <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0, x: 40 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{
+                      ease: "easeInOut",
+                      duration: 0.5,
+                      delay: index * 0.2,
+                    }}
+                    className="relative w-full h-auto"
+                  >
+                    <Image
+                      src={item.image.desktop}
+                      alt="portfolio"
+                      width={350}
+                      height={560}
+                      className="hidden md:flex object-cover w-full h-auto"
+                    />
+                    <Image
+                      src={item.image.tablet}
+                      alt="portfolio"
+                      width={573}
+                      height={240}
+                      className="md:hidden ss:flex hidden object-cover w-full h-auto"
+                    />
+                    <Image
+                      src={item.image.mobile}
+                      alt="portfolio"
+                      width={311}
+                      height={240}
+                      className="ss:hidden flex object-cover w-full h-auto"
+                    />
+                    <div className="absolute p-[30px] bottom-0 left-0 z-[4]">
+                      <h4 className="text-white font-semibold text-[22px]">
+                        {item.title}
+                      </h4>
+                      <p className="text-white">View All Projects</p>
+                    </div>
+                    <div className="w-full absolute h-full top-0 bg-black opacity-[20%] z-[3]" />
+                  </motion.div>
+                );
+              })}
+              <div className="hidden ss:flex text-[#EEEFF4] absolute top-[132px] text-[18px] left-[-132px] rotate-90 leading-none tracking-[1.2em]">
+                PORTFOLIO
+              </div>
+            </div>
+            <Footer />
+            {isScrolled && (
+              <button
+                onClick={scrollToTop}
+                className="w-[40px] h-[40px] fixed flex -rotate-90 justify-center items-center p-3 cursor-pointer bg-red md:bg-dark right-3 
+              z-[20002] bottom-10"
+              >
+                <Image
+                  src="/assets/icons/arrow-icon.svg"
+                  alt="up"
+                  width={33}
+                  height={27}
+                />
+              </button>
+            )}
+          </section>
+        )}
+      </div>
+
+    )
+    :(
+      <div className="h-full w-full relative">
       <Loader isLoading={isLoading} />
 
       {!isLoading && data && (
@@ -49,53 +134,11 @@ const Page = () => {
           } font-Spartan px-0 ss:px-10 md:px-16 md:container w-full h-full text-dark relative`}
         >
           <Header />
-          <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 gap-7 px-[30px] ss:px-16 md:px-20 mb-[200px] relative">
-            {portfolio?.map((item: any, index: number) => {
-              return (
-                <motion.div
-                  key={item.id}
-                  initial={{ opacity: 0, x: 40 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{
-                    ease: "easeInOut",
-                    duration: 0.5,
-                    delay: index * 0.2,
-                  }}
-                  className="relative w-full h-auto"
-                >
-                  <Image
-                    src={item.image.desktop}
-                    alt="portfolio"
-                    width={350}
-                    height={560}
-                    className="hidden md:flex object-cover w-full h-auto"
-                  />
-                  <Image
-                    src={item.image.tablet}
-                    alt="portfolio"
-                    width={573}
-                    height={240}
-                    className="md:hidden ss:flex hidden object-cover w-full h-auto"
-                  />
-                  <Image
-                    src={item.image.mobile}
-                    alt="portfolio"
-                    width={311}
-                    height={240}
-                    className="ss:hidden flex object-cover w-full h-auto"
-                  />
-                  <div className="absolute p-[30px] bottom-0 left-0 z-[4]">
-                    <h4 className="text-white font-semibold text-[22px]">
-                      {item.title}
-                    </h4>
-                    <p className="text-white">View All Projects</p>
-                  </div>
-                  <div className="w-full absolute h-full top-0 bg-black opacity-[20%] z-[3]" />
-                </motion.div>
-              );
-            })}
-            <div className="hidden ss:flex text-[#EEEFF4] absolute top-[132px] text-[18px] left-[-132px] rotate-90 leading-none tracking-[1.2em]">
-              PORTFOLIO
+
+          <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 gap-7 px-[30px] ss:px-16 md:px-20 mb-[270px] relative">
+            
+            <div className="hidden ss:flex text-[#354dc2] font-Spartan font-bold absolute top-[132px]  left-[-132px] rotate-90 leading-[2.5em] tracking-[1.2em]">
+              Please log in
             </div>
           </div>
           <Footer />
@@ -116,6 +159,8 @@ const Page = () => {
         </section>
       )}
     </div>
+    )}
+    </>
   );
 };
 
